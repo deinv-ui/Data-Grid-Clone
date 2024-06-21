622e4c429ee5fadd10da2b79f86370fc8bd214c1
dexin <tanx3406@gmail.com>
2024-06-21 19:44:34 +0800

    Styling and collapse menu

--- datagrid-clone/src/ProductTable.tsx
+++ datagrid-clone/src/ProductTable.tsx
@@ -1,58 +1,107 @@
-import { React, useState } from "react";
+import { useState } from "react";
 import products from "./products.json";
-interface LinkProps {
-    onClick: (event: React.MouseEvent<HTMLAnchorElement>) => void;
-    // Other props...
-  }
-export function ProductTable({ onClick, ...rest }: LinkProps) {
+
+const ProductTable: React.FC = () => {
   const headers = [
     { key: "ProductName", label: "Product" },
     { key: "ProductID", label: "ID" },
     { key: "UnitPrice", label: "Unit Price" },
     { key: "QuantityPerUnit", label: "Qty Per Unit" },
   ];
-  const [show, setShow] = useState(false);
-  const toggleShow = () => setShow(!show);
+  const [isShow, setShow] = useState<boolean[]>(
+    Array(headers.length).fill(false)
+  );
+  const [clicked, setClicked] = useState<boolean>(true);
 
+  const toggleShow = (index: number) => {
+    setShow((prev) => {
+      setClicked(!clicked);
+      const updatedVisibleRows = [...prev];
+      updatedVisibleRows[index] = !updatedVisibleRows[index];
+      return updatedVisibleRows;
+    });
+  };
   return (
-    <div className="max-h-96 mx-5 mt-10 ">
-      <table className=" table-fixed border-[1px] border-collapse text-left">
+    <div className=" mx-5 mt-10 overflow-x-auto ">
+      <table className=" divide-y divide-gray-200 table-fixed border-[1px] border-collapse text-left  min-w-full md:min-w-96 ">
         <thead className="border bg-gray-50">
           <tr className="text-left">
-            <th className="border px-1 py-4 text-gray-50">sy</th>
+            <th className="border px-1 text-gray-50">sy</th>
             {headers.map((row) => {
               return (
-                <td className=" border pl-2 pr-4" key={row.key}>
+                <td className=" border pl-2 pr-4 py-4" key={row.key}>
                   {row.label}
                 </td>
               );
             })}
           </tr>
         </thead>
-        <tbody className="h-96 overflow-y-scroll bg-white">
-          {products.map((product) => {
+        <tbody className="h-96 overflow-y-scroll ">
+          {products.map((product, index) => {
             return (
               <>
-                <tr className="hover:bg-tablehover" key={product.ProductID}>
-                <td className="border pl-2 py-3">
-                    <a role="button" onClick={onClick} title="Toggle Show">
-                        <span>
-                            <svg
-                                aria-hidden="true"
-                                focusable="false"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 512 512"
-                            >
-                                <path d="M288 224V96h-64v128H96v64h128v128h64V288h128v-64z"></path>
-                            </svg>
-                        </span>
+                <tr
+                  key={product.ProductID}
+                  className={
+                    index % 2 === 0
+                      ? "bg-white hover:bg-gray-100"
+                      : "bg-gray-50 hover:bg-gray-200"
+                  }
+                >
+                  <td className="border pl-2 py-5  ">
+                    <a
+                      key={product.ProductID}
+                      role="button"
+                      href="#"
+                      onClick={() => toggleShow(index)}
+                      title="Toggle Show"
+                    >
+                      <span>
+                        {!isShow[index] ? (
+                          <svg
+                            aria-hidden="true"
+                            focusable="false"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                          >
+                            <path d="M288 224V96h-64v128H96v64h128v128h64V288h128v-64z"></path>
+                          </svg>
+                        ) : (
+                          <svg
+                            aria-hidden="true"
+                            focusable="false"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                          >
+                            <path d="M96 224v64h320v-64z"></path>
+                          </svg>
+                        )}
+                      </span>
                     </a>
-                </td>
+                  </td>
                   <td className="border pl-2 ">{product.ProductName} </td>
                   <td className="border pl-2">{product.ProductID} </td>
                   <td className="border pl-2">{product.UnitPrice} </td>
                   <td className="border pl-2">{product.QuantityPerUnit} </td>
                 </tr>
+                {isShow[index] && (
+                  <tr className="hover:bg-gray-200">
+                    <td className="border pl-2 "> </td>
+                    <td className="border pl-2 ">
+                      <span className="font-bold">In Stock: </span>
+                      {product.UnitsInStock} <br />
+                      <span className="font-bold">On Order: </span>
+                      {product.UnitsOnOrder} <br />
+                      <span className="font-bold">Reorder Level: </span>
+                      {product.ReorderLevel} <br />
+                      <span className="font-bold">Discontinued: </span>
+                      {product.ReorderLevel} <br />
+                      <span className="font-bold">Category: </span>
+                      {product.Category.CategoryName}-
+                      {product.Category.Description} <br />
+                    </td>
+                  </tr>
+                )}
               </>
             );
           })}
@@ -62,3 +111,4 @@ export function ProductTable({ onClick, ...rest }: LinkProps) {
   );
 };
 
+export default ProductTable;
